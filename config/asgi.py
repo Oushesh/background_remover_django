ccc9f2cc3b322a2adad5e3cde8c6f3722101c53f
oushesh <oushesh@gmail.com>
2023-09-05 17:13:31 +0400

    added requirements.txt

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'background_remover.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bgremove.settings')
 
 application = get_asgi_application()
